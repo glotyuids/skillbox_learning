4242eddead0105e40119c32309d42bfed8ddb602
Валько Дмитрий <nosefu@gmail.com>
2020-01-30 19:30:57 +0300

    Нулевое задание

--- lesson_002/00_distance.py
+++ lesson_002/00_distance.py
@@ -14,7 +14,25 @@ sites = {
 
 distances = {}
 
-# TODO здесь заполнение словаря
+
+def calculate_distance(cite1, cite2):
+    distance = ((cite1[0] - cite2[0]) ** 2 + (cite1[1] - cite2[1]) ** 2) ** 0.5
+    return distance
+
+
+moscow_london = calculate_distance(sites['Moscow'], sites['London'])
+moscow_paris  = calculate_distance(sites['Moscow'], sites['Paris'])
+london_paris  = calculate_distance(sites['London'], sites['Paris'])     # Голуби вверх, блики крыш...
+
+distances['Moscow'] = {'London': moscow_london,
+                       'Paris' : moscow_paris}
+
+distances['London'] = {'Moscow': moscow_london,
+                       'Paris' : london_paris}
+
+distances['Paris']  = {'Moscow': moscow_paris,
+                       'London': london_paris}
+
 
 print(distances)
 
