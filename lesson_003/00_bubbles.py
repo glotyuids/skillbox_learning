2d9cc9751f0bcbe3eb1f0dc1a6bce77da01d3f2d
Валько Дмитрий <nosefu@gmail.com>
2020-02-05 23:19:12 +0300

    Модуль 3. Задание 0. Написал функцию рисования пузырей

--- lesson_003/00_bubbles.py
+++ lesson_003/00_bubbles.py
@@ -11,8 +11,34 @@ for _ in range(3):
     sd.circle(circle_center, radius, width=2)
     radius += 5
 
+
 # Написать функцию рисования пузырька, принммающую 3 (или более) параметра: точка рисования, шаг и цвет
-# TODO здесь ваш код
+def draw_bubble(center, inner_radius=60, step=5, color=(255, 255, 0), line_width=2):
+    """Draws a bubble of three circles.
+
+    Parameters
+    ----------
+    center : simple_draw Point
+        Center of bubble
+
+    inner_radius : int, default=60
+        Radius of inner circle
+
+    step : int, default=5
+        Step of increasing the radius of the circles
+
+    color : tuple(int, int, int), default=(255, 255, 0)
+        Color of circles. Accepts tuple(red, green, blue),
+        where color value between 0 and 255, including both of them
+
+    line_width : int, default=2
+        Circle line width
+
+    """
+    for _ in range(3):
+        sd.circle(center, inner_radius, color, line_width)
+        inner_radius += step
+
 
 # Нарисовать 10 пузырьков в ряд
 # TODO здесь ваш код
