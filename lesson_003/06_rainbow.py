67fbd5075590eeafdf88fdd42cc8da2930ded1ea
Valko Dmitry <nosefu@gmail.com>
2020-02-06 12:10:00 +0300

    Модуль 3. Задание 6. Радуга дугами

--- lesson_003/06_rainbow.py
+++ lesson_003/06_rainbow.py
@@ -9,10 +9,16 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
 
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
 # TODO здесь ваш код
+sd.resolution = (1200, 600)
 
 # Усложненное задание, делать по желанию.
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
 # поэкспериментировать с параметрами, что бы было красиво
-# TODO здесь ваш код
+circles_center = sd.get_point(000, -700)
+radius = 1200
+line_width = 40
+for color in rainbow_colors:
+    sd.circle(circles_center, radius, color, width=line_width)
+    radius += line_width
 
 sd.pause()
