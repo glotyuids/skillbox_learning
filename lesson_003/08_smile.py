a63f74cf010598b6475188f17019043ff3881ecc
Валько Дмитрий <nosefu@gmail.com>
2020-02-06 20:37:44 +0300

    Модуль 3. Задание 8. Дорисованы глаза, брови, борода. Смайлик готов

--- lesson_003/08_smile.py
+++ lesson_003/08_smile.py
@@ -10,11 +10,11 @@ import simple_draw
 
 
 def draw_smile(x, y, color=simple_draw.COLOR_YELLOW):
-    pass
-#   TODO Здесь собираем морду
     scale = 10
     draw_head(x, y + 13, scale, color)
-    draw_mouth(x + 1, y + 12, scale)
+    draw_eyes(x + 4, y + 22, scale)
+    draw_beard(x, y, scale)
+    draw_mouth(x + 1, y + 13, scale)
 
 
 def draw_head(x, y, scale=1, color=simple_draw.COLOR_YELLOW):
@@ -31,7 +31,6 @@ def draw_head(x, y, scale=1, color=simple_draw.COLOR_YELLOW):
 
 
 def draw_mouth(x, y, scale=1):
-#   TODO Здесь рисуем рот
     # Outline
     left_bottom_x = x * scale
     left_bottom_y = (y + 4) * scale
@@ -98,19 +97,103 @@ def draw_mouth(x, y, scale=1):
     simple_draw.rectangle(left_bottom, right_top, simple_draw.COLOR_BLACK, 0)
 
 
-def draw_eyes(x, y):
-    pass
-#   TODO Здесь рисуем глаза
+def draw_eyes(x, y, scale=1):
+    # Left eye
+    for i in range(2):
+        left_bottom_x = (x + i) * scale
+        left_bottom_y = y * scale
+        left_bottom = simple_draw.get_point(left_bottom_x, left_bottom_y)
+
+        right_top_x = (x + 3) * scale
+        right_top_y = (y + 3 + i) * scale
+        right_top = simple_draw.get_point(right_top_x, right_top_y)
+
+        simple_draw.rectangle(left_bottom, right_top, simple_draw.COLOR_WHITE, 0)
+
+    # Left pupil
+    left_bottom_x = (x + 1) * scale
+    left_bottom_y = (y + 1) * scale
+    left_bottom = simple_draw.get_point(left_bottom_x, left_bottom_y)
+
+    right_top_x = (x + 3) * scale
+    right_top_y = (y + 3) * scale
+    right_top = simple_draw.get_point(right_top_x, right_top_y)
+
+    simple_draw.rectangle(left_bottom, right_top, simple_draw.COLOR_BLACK, 0)
+
+    # Right eye
+    for i in range(2):
+        left_bottom_x = (x + 4) * scale
+        left_bottom_y = y * scale
+        left_bottom = simple_draw.get_point(left_bottom_x, left_bottom_y)
+
+        right_top_x = (x + 6 + i) * scale
+        right_top_y = (y + 4 - i) * scale
+        right_top = simple_draw.get_point(right_top_x, right_top_y)
+
+        simple_draw.rectangle(left_bottom, right_top, simple_draw.COLOR_WHITE, 0)
+
+    # Right pupil
+    left_bottom_x = (x + 4) * scale
+    left_bottom_y = (y + 1) * scale
+    left_bottom = simple_draw.get_point(left_bottom_x, left_bottom_y)
+
+    right_top_x = (x + 6) * scale
+    right_top_y = (y + 3) * scale
+    right_top = simple_draw.get_point(right_top_x, right_top_y)
+
+    simple_draw.rectangle(left_bottom, right_top, simple_draw.COLOR_BLACK, 0)
+
+    # Left eyebrow
+    left_bottom_x = x * scale
+    left_bottom_y = (y + 4) * scale
+    left_bottom = simple_draw.get_point(left_bottom_x, left_bottom_y)
+
+    right_top_x = (x + 3) * scale
+    right_top_y = (y + 5) * scale
+    right_top = simple_draw.get_point(right_top_x, right_top_y)
+
+    simple_draw.rectangle(left_bottom, right_top, simple_draw.COLOR_BLACK, 0)
+
+    # Right eyebrow
+    left_bottom_x = (x + 5) * scale
+    left_bottom_y = (y + 5) * scale
+    left_bottom = simple_draw.get_point(left_bottom_x, left_bottom_y)
+
+    right_top_x = (x + 7) * scale
+    right_top_y = (y + 6) * scale
+    right_top = simple_draw.get_point(right_top_x, right_top_y)
+
+    simple_draw.rectangle(left_bottom, right_top, simple_draw.COLOR_BLACK, 0)
+
+    left_bottom_x = (x + 7) * scale
+    left_bottom_y = (y + 4) * scale
+    left_bottom = simple_draw.get_point(left_bottom_x, left_bottom_y)
+
+    right_top_x = (x + 8) * scale
+    right_top_y = (y + 5) * scale
+    right_top = simple_draw.get_point(right_top_x, right_top_y)
+
+    simple_draw.rectangle(left_bottom, right_top, simple_draw.COLOR_BLACK, 0)
 
 
-def draw_beard(x, y):
-    pass
-#   TODO Здесь рисуем бороду
+def draw_vertical_rectangle (x, y_top, y_bottom, color=simple_draw.COLOR_YELLOW, scale=1):
+    left_bottom = simple_draw.get_point(x * scale, y_bottom * scale)
+    right_top = simple_draw.get_point((x + 1) * scale, y_top * scale)
+    simple_draw.rectangle(left_bottom, right_top, color, 0)
+
+
+def draw_beard(x, y, scale=1):
+    beard = [5, 7, 8, 12, 16, 15, 18, 20, 19, 17, 14, 13, 10, 8, 7, 4]
+    hair_y_top = y + 20
+    for i, hair in enumerate(beard):
+        hair_x = x + i
+        hair_y_bottom = hair_y_top - hair
+        draw_vertical_rectangle(hair_x, hair_y_top, hair_y_bottom, simple_draw.COLOR_WHITE, scale)
+
 
 simple_draw.resolution = (1200, 600)
 
 draw_smile(10, 10)
-
-
 
 simple_draw.pause()
