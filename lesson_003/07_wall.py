eed13f6149c31a1703bc53e659b8743a93ab337b
Валько Дмитрий <nosefu@gmail.com>
2020-02-08 01:50:50 +0300

    Модуль 3. Задание 7. Упростил код. Избавился от хардкода. Для наглядности изменил оформление кирпичей

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -5,10 +5,11 @@ import simple_draw
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
+BRICK_WIDTH = 100
+BRICK_HEIGHT = 50
+simple_draw.resolution = (1262, 615)
 
-simple_draw.resolution = (1200, 600)
-
-# TODO Для создная гибкого кода важно как можно меньше использовать харкод - числа, строки, а вмест них использовать
+# Для создная гибкого кода важно как можно меньше использовать харкод - числа, строки, а вмест них использовать
 #  константы и/или программые способы их получения:
 #  1) введите константы размеров кирпича
 #  2) там где нужны размеры окна вывода используйте sd.resolution
@@ -31,43 +32,38 @@ def draw_brick(x, y, width=100, height=50):
     """
     left_bottom = simple_draw.get_point(x, y)
     right_top = simple_draw.get_point(x + width, y + height)
-    simple_draw.rectangle(left_bottom, right_top, width=1)
+    simple_draw.rectangle(left_bottom, right_top, color=simple_draw.COLOR_DARK_RED, width=0)
+    simple_draw.rectangle(left_bottom, right_top, color=simple_draw.COLOR_BLACK, width=1)
 
 
-def draw_brick_line (x, y, count):
+def draw_brick_line(x, y, line_width, brick_width=100, brick_height=50):
     """Draws line of bricks
 
     Parameters
     ----------
+
+
     x, y : int
         Bottom left point of first brick
 
-    count : int
-        Count of bricks in line
+    line_width : int
+        Width of bricks line in pixels
+
+    brick_width : int, default=100
+        Width of brick
+
+    brick_height : int, default=50
+        Height of brick
 
     """
-    next_x = x
-    for _ in range(count):
-        draw_brick(next_x, y)
-        next_x += 100
-        # TODO Цикл по иксу от нуля до ширины окна с шагом в ширину кирпича и не придётся ничего считать ни до цикла
-        #  ни внутри него
+    # TODO Здесь к конечной точке (второй аргумент range) добавил ширину кирпича, чтобы был заполнен весь экран.
+    #  Если этого не сделать, то в смещённых по x строках не будет хватать одного кирпича. Можете проверить
+    for x in range(x, x + line_width + brick_width, brick_width):
+        draw_brick(x, y, brick_width, brick_height)
 
 
-wall_width = simple_draw.resolution[0] // 100 + 1   # +1 для того, чтобы при смещении на полкирпича всё было корректно
-wall_height = simple_draw.resolution[1] // 50 + 1   # +1 на случай, если размер окна не кратен размеру кирпича
-# TODO Не вполне понял зачем +1, без этого никогда не было проблем
-
-for i in range(wall_height):
-    line_y = i * 50
-    # TODO всё-таки простая итерация по игреку с range от нуля до высоты окна с шагом высоты кирпича намного прощце
-    #  и легче читается
-    if i % 2 == 0:
-       line_x = 0
-       # TODO Точнее "сдвиг_по_икс"
-    else:
-        line_x = -50
-    # TODO Рекомендую для вычисления сдвига использовать тернарный оператор, он идеально сюда подходит
-    draw_brick_line(line_x, line_y, wall_width)
+for y in range(0, simple_draw.resolution[1], BRICK_HEIGHT):
+    x = 0 if y % (BRICK_HEIGHT * 2) == 0 else -BRICK_WIDTH // 2
+    draw_brick_line(x, y, simple_draw.resolution[0], BRICK_WIDTH, BRICK_HEIGHT)
 
 simple_draw.pause()
