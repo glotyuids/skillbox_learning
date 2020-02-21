0e0495e269906e8bc5c048fd5d455e8fadd77a60
Валько Дмитрий <nosefu@gmail.com>
2020-02-21 22:08:42 +0300

    Модуль 4. Задание 5. Написал код генерации рандомных снежинок

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+from pprint import pprint
 
 import simple_draw as sd
+import random
 
 
 # На основе кода из практической части реализовать снегопад:
@@ -17,17 +19,62 @@ N = 20
 # sd.random_number()
 # sd.user_want_exit()
 
-# TODO здесь ваш код
-while True:
-    sd.clear_screen()
-    pass
-    pass
-    pass
-    sd.sleep(0.1)
-    if sd.user_want_exit():
-        break
 
-sd.pause()
+def remap_range(value, in_min, in_max, out_min, out_max):
+    """Функция пропорционально переносит значение (value) из текущего диапазона значений (fromLow .. fromHigh)
+            в новый диапазон (toLow .. toHigh), заданный параметрами.
+
+    """
+    return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+
+def generate_snowflake():
+    sf_length = random.randint(MIN_LENGTH, MAX_LENGTH)
+    return {
+            'x': random.randint(0, sd.resolution[0]),
+            'y': sd.resolution[1] + MAX_LENGTH,
+            'length': sf_length,
+            'factor_a': round(random.uniform(0.2, 1), 2),
+            'factor_b': round(random.uniform(0.1, 1), 2),
+            'factor_c': round(random.randint(20, 90), 2),
+            'h_speed': random.randint(0, 30),
+            'v_speed': round(remap_range(value=sf_length, in_min=MIN_LENGTH, in_max=MAX_LENGTH, out_min=2, out_max=60))
+        }
+
+
+MAX_LENGTH = 100
+MIN_LENGTH = 10
+
+# генерим первоначальный список снежинок
+blizzard = []
+for _ in range(N):
+    blizzard.append(generate_snowflake())
+# pprint(blizzard)
+
+# sd.resolution = (600, 1200)
+# while True:
+#     sd.clear_screen()
+#
+#     for snowflake in blizzard:
+#         point = sd.get_point(snowflake['x'], snowflake['y'])
+#         sd.snowflake(
+#             center=point,
+#             length=snowflake['length'],
+#             factor_a=snowflake['factor_a'],
+#             factor_b=snowflake['factor_b'],
+#             factor_c=snowflake['factor_c'],
+#         )
+#         snowflake['y'] -= snowflake['v_speed']
+#         if snowflake['y'] < snowflake['length']:
+#             blizzard
+#         x = x + 30
+#
+#
+#     sd.sleep(0.1)
+#     if sd.user_want_exit():
+#         break
+#
+# sd.pause()
 
 # подсказка! для ускорения отрисовки можно
 #  - убрать clear_screen()
