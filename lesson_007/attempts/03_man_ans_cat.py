38a51eea428a787fac02ae3fb2cace509e9a7ce1
Валько Дмитрий <nosefu@gmail.com>
2020-04-01 14:04:12 +0300

    Модуль 7. Задание 3. Добавил ответ

--- lesson_007/attempts/03_man_ans_cat.py
+++ lesson_007/attempts/03_man_ans_cat.py
@@ -28,80 +28,124 @@ from random import randint, choice
 from termcolor import cprint
 
 
+CAT_NAMES = ['Феликс', 'Том', 'Сильвестр', 'Гарфилд', 'в сапогах', 'Чешир', 'Артемис', 'Мяут', 'Котобус', 'Мистер Кэт']
+
+
 class Man:
 
-    def __init__(self, name):
+    def __init__(self, name, desired_number_of_cats=0):
         self.name = name
         self.fullness = 50
         self.house = None
-        self.desired_cats_number = 0
+        self.desired_number_of_cats = desired_number_of_cats
+        # TODO для оптимизации жизни добавим человеку намерения.они нужны на случай перекрывающихся потребностей:
+        #  например, человек хочет есть, но нет еды. В предыдущей реализации
+        self.intentions = []
 
     def __str__(self):
-        return 'Я - {}, сытость {}'.format(
-            self.name, self.fullness)
+        return f'Я - {self.name}, сытость {self.fullness}, хочу еще {self.desired_number_of_cats} котов'
 
-    #  Если человек хочет есть и видит, что еды нет, то он в тот же день идёт в магазин
     def eat(self):
         if self.house.food >= 10:
             cprint('{} поел'.format(self.name), color='yellow')
             self.fullness += 10
             self.house.food -= 10
+            if self.intentions and self.intentions[-1] == 'eat':
+                del self.intentions[-1]
         else:
             cprint('{} нет еды'.format(self.name), color='red')
-            self.buy_food()
+            # self.fullness -= 10
+            self.intentions.append('buy_food')
+            self.shopping(min_food=30, min_cat_food=self.house.cats_number * 20)
 
     def work(self):
         cprint('{} сходил на работу'.format(self.name), color='blue')
         self.house.money += 150
         self.fullness -= 10
+        if self.intentions and self.intentions[-1] == 'work':
+            del self.intentions[-1]
 
     def watch_MTV(self):
         cprint('{} смотрел MTV целый день'.format(self.name), color='green')
         self.fullness -= 10
+        if self.intentions and self.intentions[-1] == 'watch_mtv':
+            del self.intentions[-1]
 
-    #  Если нет денег для похода в магазин (за едой или кормом), то человек в тот же день идёт на работу
-    #  В таких цепочках главное, чтобы не получилось неявной рекурсии через вторую/третью функцию
-    # TODO Да, я это прекрасно понимаю. Для того, чтобы обойти эту проблему (а также решить ещё парочку),
-    #  я пробовал реализовать у человека систему намерений: стек, в который попадают все его хотелки,
-    #  а уже оттуда при выполнении act() они бы берутся и выполняются. Убил часов 5-6, и понял,
-    #  что придётся потратить ещё кучу времени для доведения этой системы до ума,
-    #  поскольку нужна была приоритизация хотелок, но приоритизация умная,
-    #  которая учитывала бы отсутствие еды, денег и т.д. и позволяла бы делать несколько действий в один день.
-    #  Если вдруг интересно до чего я добрался, то закину в папку lesson_007/attempts то, на чём я остновился.
+    def shopping(self, min_food=0, min_cat_food=0):
+        # TODO Оптимизировал процесс закупок. Теперь человек закупается в одном торговом центре,
+        #  а значит покупая один ресурс он может попутно докупить и другой.
 
-    #  TODO Короче, я понял, что двигаюсь куда-то не туда, сбросил все изменения до последнего коммита
-    #   и переписал всё по новой. Дубовее, но проще
-    def buy_food(self):
+        # TODO Энергия тратится только если человек дошёл до магазина.
+        #  Если денег не хватило, то энергия не тратится - человек пересчитал их дома до похода в магазин
+        before_shopping_money = self.house.money
+
+        if self.intentions[-1] == 'buy_cat_food':
+            if self.house.cat_food < min_cat_food:
+                self.buy_cat_food(shopping_mall=True)
+            if self.house.food < min_food + 20:
+                self.buy_food(shopping_mall=True)
+        else:
+            if self.house.food < min_food:
+                self.buy_food(shopping_mall=True)
+            if self.house.cat_food < min_cat_food + 20:
+                self.buy_cat_food(shopping_mall=True)
+
+        if self.house.money != before_shopping_money:
+            self.fullness -= 10
+        else:
+            self.intentions.append('work')
+            self.work()
+
+    def buy_cat_food(self, shopping_mall=False):
+
+        want_to_buy = self.house.cats_number * 20
+        price = want_to_buy
+
+        if price <= self.house.money < price * 2:
+            cprint('{} сходил в зоомагазин и купил {} кошачьего корма'
+                   .format(self.name, want_to_buy), color='magenta')
+            self.house.money -= price
+            self.house.cat_food += want_to_buy
+            if not shopping_mall:
+                self.fullness -= 10
+            if self.intentions and self.intentions[-1] == 'buy_cat_food':
+                del self.intentions[-1]
+
+        elif self.house.money >= price * 3:
+            cprint('{} сходил в зоомагазин и купил {} кошачьего корма'
+                   .format(self.name, want_to_buy * 2), color='magenta')
+            self.house.money -= price * 2
+            self.house.cat_food += want_to_buy * 2
+            if not shopping_mall:
+                self.fullness -= 10
+            if self.intentions and self.intentions[-1] == 'buy_cat_food':
+                del self.intentions[-1]
+        else:
+            cprint('{} на кошачий корм денег нет!'.format(self.name), color='red')
+            if self.intentions[-1] != 'work':
+                self.intentions.append('work')
+
+    def buy_food(self, shopping_mall=False):
         if 50 <= self.house.money < 100:
             cprint('{} сходил в магазин за едой'.format(self.name), color='magenta')
             self.house.money -= 50
-            self.house.food += 50
-            self.fullness -= 10
-        elif self.house.money >= 100:
+            self.house.cat_food += 50
+            if not shopping_mall:
+                self.fullness -= 10
+            if self.intentions and self.intentions[-1] == 'buy_food':
+                del self.intentions[-1]
+        elif self.house.money >= 150:
             cprint('{} купил целый ящик доширака'.format(self.name), color='magenta')
             self.house.money -= 100
-            self.house.food += 100
-            self.fullness -= 10
+            self.house.cat_food += 100
+            if not shopping_mall:
+                self.fullness -= 10
+            if self.intentions and self.intentions[-1] == 'buy_food':
+                del self.intentions[-1]
         else:
             cprint('{} денег на еду нет!'.format(self.name), color='red')
-            #  Люди не умирали от голода, поскольку во время голода сытость не изменялась. Исправил
-            # Отлично, что вы сами это оттестировали и нашли
-            self.work()
-
-    def buy_cat_food(self):
-        if 50 <= self.house.money < 100:
-            cprint('{} сходил в зоомагазин за кошачьим кормом'.format(self.name), color='magenta')
-            self.house.money -= 50
-            self.house.cat_food += 50
-            self.fullness -= 10
-        elif self.house.money >= 100:
-            cprint('{} купил много кошачьего корма'.format(self.name), color='magenta')
-            self.house.money -= 100
-            self.house.cat_food += 100
-            self.fullness -= 10
-        else:
-            cprint('{} на кошачий корм денег нет!'.format(self.name), color='red')
-            self.work()
+            if self.intentions[-1] != 'work':
+                self.intentions.append('work')
 
     def clean_house(self):
         if self.house.dirtiness >= 500 and self.house.money >= 50:
@@ -110,6 +154,8 @@ class Man:
             cprint('{} убрался в доме'.format(self.name), color='magenta')
             self.fullness -= 10
             self.house.dirtiness -= 100
+        if self.intentions and self.intentions[-1] == 'clean_house':
+            del self.intentions[-1]
 
     def call_cleaning(self):
         if self.house.money >= 50:
@@ -129,41 +175,72 @@ class Man:
         #  Поэтому предположим, что приют находится прямо напротив дома
         # Любое действие отнимает 10 от сытости
         self.fullness -= 10
-        self.desired_cats_number -= 1
-
+        self.desired_number_of_cats -= 1
+        self.house.cats_number += 1
+        if self.intentions and self.intentions[-1] == 'take_new_cat':
+            del self.intentions[-1]
         cprint('{} Взял кота из приюта'.format(self.name), color='cyan')
-        return Cat(house=self.house)
+        return Cat(name=choice(CAT_NAMES), house=self.house)
 
     def act(self, my_cats):
+        # + work
+        # + eat
+        # + buy_food
+        # + buy_cat_food
+        # + watch_mtv
+        # + clean_house
+        # + take_new_cat
         if self.fullness <= 0:
             cprint('{} умер...'.format(self.name), color='red')
             quit(1)
         dice = randint(1, 6)
+
+
         if self.fullness < 20:
-            self.eat()
+            self.intentions.append('eat')
         elif self.house.food < 30:
-            self.buy_food()
+            self.intentions.append('buy_food')
         elif self.house.money < 50:
-            self.work()
-        elif self.house.cat_food < len(cats) * 20:
-            self.buy_cat_food()
-        elif self.house.dirtiness > 100:
-            self.clean_house()
-        #  Теперь человек берёт нового кота не сразу, а когда у него есть такая возможность
-        elif self.desired_cats_number > 0:
-            my_cats.append(self.take_cat_from_animal_shelter())
-        elif 1 <= dice <= 3:
-            self.work()
-        elif dice == 4:
-            self.eat()
-        else:
-            self.watch_MTV()
+            self.intentions.append('work')
+
+        if not self.intentions:
+            if self.house.cat_food < self.house.cats_number * 20:
+                self.intentions.append('buy_cat_food')
+            elif self.house.dirtiness > 100:
+                self.intentions.append('clean_house')
+            elif self.desired_number_of_cats > 0:
+                self.intentions.append('take_new_cat')
+            elif 1 <= dice <= 3:
+                self.intentions.append('work')
+            elif dice == 4:
+                self.intentions.append('eat')
+            else:
+                self.intentions.append('watch_mtv')
+
+        print(f'Намерения: {self.intentions}')
+
+        if self.intentions:
+            if self.intentions[-1] == 'work':
+                self.work()
+            elif self.intentions[-1] == 'eat':
+                self.eat()
+            elif self.intentions[-1] == 'buy_food':
+                self.shopping(min_food=30, min_cat_food=self.house.cats_number * 20)
+            elif self.intentions[-1] == 'buy_cat_food':
+                self.shopping(min_food=30, min_cat_food=self.house.cats_number * 20)
+            elif self.intentions[-1] == 'watch_mtv':
+                self.watch_MTV()
+            elif self.intentions[-1] == 'clean_house':
+                self.clean_house()
+            elif self.intentions[-1] == 'take_new_cat':
+                my_cats.append(self.take_cat_from_animal_shelter())
 
 
 class House:
 
     def __init__(self):
         self.food = 50
+        self.cats_number = 0
         self.cat_food = 0
         self.dirtiness = 0
         self.money = 0
@@ -185,12 +262,7 @@ class House:
 
 
 class Cat:
-    #  Сделал имена кошек константой класса Cat.
-    NAMES = ['Феликс', 'Том', 'Сильвестр', 'Гарфилд', 'в сапогах', 'Чешир', 'Артемис', 'Мяут', 'Котобус', 'Мистер Кэт']
-
-    def __init__(self, house, name=None):
-        if name is None:
-            name = choice(Cat.NAMES)
+    def __init__(self, name, house):
         self.name = name
         self.fullness = 50
         self.house = house
@@ -242,29 +314,35 @@ class Cat:
             self.sleep()
 
 
-beavis = Man(name='Бивис')
+beavis = Man(name='Бивис', desired_number_of_cats=100)
 
 my_sweet_home = House()
 beavis.go_to_the_house(house=my_sweet_home)
-beavis.desired_cats_number = 4
 cats = [beavis.take_cat_from_animal_shelter()]
+day_last_cat_taken = 0
+current_cats_number = 1
 
 for day in range(1, 366):
     print('================ день {} =================='.format(day))
-    beavis.act(my_cats=cats)
+    beavis.act(cats)
     for cat in cats:
         cat.act()
     my_sweet_home.check_status()
+
+    if current_cats_number < my_sweet_home.cats_number:
+        current_cats_number = my_sweet_home.cats_number
+        day_last_cat_taken = day
+
     print('--- в конце дня ---')
     print(beavis)
     for cat in cats:
         print(cat)
     print(my_sweet_home)
+    print('Последнего кота заселили на {} день'.format(day_last_cat_taken))
+
 
 # Усложненное задание (делать по желанию)
 # Создать несколько (2-3) котов и подселить их в дом к человеку.
 # Им всем вместе так же надо прожить 365 дней.
 
 # (Можно определить критическое количество котов, которое может прокормить человек...)
-
-# зачет!
