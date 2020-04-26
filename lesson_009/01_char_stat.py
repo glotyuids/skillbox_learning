4e51f72b8670a7f2108d87d3c19813deedad005f
Valko Dmitry <nosefu@gmail.com>
2020-04-26 13:31:21 +0300

    Модуль 9. Задание 1. Выполнил основное задание

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -21,7 +21,59 @@
 # Упорядочивание по частоте - по убыванию. Ширину таблицы подберите по своему вкусу
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
-# TODO здесь ваш код
+import os
+from pprint import pprint
+
+
+class BaseTextAnalyzer():
+    def __init__(self, filename):
+        self.processed_data = []
+        self.filename = os.path.normpath(filename)
+
+    def analyze(self):
+        self.prepare_file()         # может его распаковать надо будет. а может сконвертить из одного формата в другой
+        self.process_data()         # собираем статистику
+        self.postprocess_data()     # обработка данных. В данном случае та или иная сортировка
+        self.output_data()          # выводим данные (в консоль, файл и т.д. - зависит от метода)
+        
+    def prepare_file(self):
+        pass
+
+    def process_data(self):
+        stats = {}
+        with open(self.filename, mode='r', encoding='cp1251') as file:
+            for line in file:
+                for char in line[:-1]:
+                    if char.isalpha():
+                        if char in stats:
+                            stats[char] += 1
+                        else:
+                            stats[char] = 1
+        self.processed_data = [[count, char] for char, count in stats.items()]
+
+    def postprocess_data(self):
+        pass
+
+    def output_data(self):
+        print('╔═════════╤══════════╗ \n'
+              '║  буква  │ частота  ║ \n'
+              '╟─────────┼──────────╢')
+
+        for count, char in self.processed_data:
+            print(f'║{char:^9}│{count:8d}  ║')
+
+        print('╟─────────┼──────────╢')
+
+        chars_count = sum([count for count, _ in self.processed_data])
+        print(f'╟  итого  │{chars_count:8d}  ║')
+
+        print('╚═════════╧══════════╝')
+
+
+filename = 'python_snippets/voyna-i-mir.txt'
+
+analyzer = BaseTextAnalyzer(filename)
+analyzer.analyze()
 
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
