2379ecd07b4d5fdf46da2e14305bdb3241067261
Valko Dmitry <nosefu@gmail.com>
2020-04-26 15:30:24 +0300

    Модуль 9. Задание 2. Увлёкся и выполнил оба этапа

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -19,7 +19,77 @@
 # Входные параметры: файл для анализа, файл результата
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
-# TODO здесь ваш код
+import os
+
+
+class BaseLogAnalyzer:
+    time_slice = [1, 17]
+
+    def __init__(self, log_file, results_file):
+        self.log_file = os.path.normpath(log_file)
+        self.results_file = os.path.normpath(results_file)
+
+    def analyze(self):
+        self.prepare_file()  # может лог распаковать надо будет. а может привести к нужному формату
+        self.process_data()  # обрабатываем лог
+        self.postprocess_data()  # постобработка данных (фильтрация например)
+        self.postprocess_result_file()
+        # может результаты в архив упаковать надо будет, приводить к нужному формату или просто вывести на консоль
+
+    def prepare_file(self):
+        pass
+
+    def process_data(self):
+        with open(self.log_file, mode='r', encoding='utf-8') as log:
+            with open(self.results_file, mode='w', encoding='utf-8') as results:
+                current_time = log.read(self.time_slice[1])
+                current_time = current_time[self.time_slice[0]: self.time_slice[1]]
+                log.seek(0, 0)
+                current_minute_NOKs = 0
+                for line in log:
+                    if current_time != line[self.time_slice[0]: self.time_slice[1]]:
+                        results.write(f'[{current_time}] {current_minute_NOKs}\n')
+                        current_time = line[self.time_slice[0]: self.time_slice[1]]
+                        current_minute_NOKs = 0
+                    if 'NOK' in line:
+                        current_minute_NOKs += 1
+                results.write(f'[{current_time}] {current_minute_NOKs}\n')
+
+    def postprocess_data(self):
+        pass
+
+    def postprocess_result_file(self):
+        pass
+
+
+class GroupByHour:
+    time_slice = [1, 14]
+
+
+class GroupByMonth:
+    time_slice = [1, 8]
+
+
+class GroupByYear:
+    time_slice = [1, 5]
+
+class PrintResults:
+    def postprocess_result_file(self):
+        with open(self.results_file, mode='r', encoding='utf-8') as results:
+            for line in results:
+                print(line, end='')
+
+
+class UserAnalyzer(GroupByHour, PrintResults, BaseLogAnalyzer):
+    pass
+
+
+log_file = 'events.txt'
+results_file = 'results.txt'
+
+log_analyzer = UserAnalyzer(log_file=log_file, results_file=results_file)
+log_analyzer.analyze()
+
 
 # После выполнения первого этапа нужно сделать группировку событий
 #  - по часам
