cab77bda54ed8a9ae30b1b8457d3709efe225121
Valko Dmitry <nosefu@gmail.com>
2020-05-03 20:34:28 +0300

    Модуль 10. Задание 3. Отделил зёрна от плевел)

--- lesson_010/03_registration_log.py
+++ lesson_010/03_registration_log.py
@@ -22,4 +22,43 @@
 # - поле возраст НЕ является числом от 10 до 99: ValueError
 # Вызов метода обернуть в try-except.
 
-# TODO здесь ваш код
+INPUT_RECORDS = 'registrations.txt'
+VALID_RECORDS = 'registrations_good.log'
+INVALID_RECORDS = 'registrations_bad.log'
+
+
+# Для обоих кастомных классов хорошо подходит ValueError: поддерживаемый тип, неподдерживаемое значение
+class NotEmailError(ValueError):
+    def __str__(self):
+        return f'must include an @ and a dot, got {self.args[0]}'
+
+
+class NotNameError(ValueError):
+    def __str__(self):
+        return f'must only contain the letters, got {self.args[0]}'
+
+
+def check_record(string):
+    name, email, age = string.split()
+    if not name.isalpha():
+        raise NotNameError(name)
+    if ('@' not in email or
+            '.' not in email[email.find('@') + 1:]):
+        raise NotEmailError(email)
+    if int(age) not in range(10, 100):
+        raise ValueError(f'invalid age (expected from 10 to 99, got {age})')
+
+
+with open(INPUT_RECORDS, mode='r', encoding='utf8') as records:
+    with open(VALID_RECORDS, mode='w', encoding='utf8') as valids:
+        with open(INVALID_RECORDS, mode='w', encoding='utf8') as invalids:
+            for line in records:
+                line = line[:-1]
+                try:
+                    check_record(line)
+                except (ValueError, NotEmailError, NotNameError) as exc:
+                    print(f'{exc.__class__.__name__}: {exc}: {line}')
+                    invalids.write(line + '\n')
+                else:
+                    valids.write(line + '\n')
+
