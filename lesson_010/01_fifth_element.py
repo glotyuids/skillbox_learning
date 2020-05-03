ba0bede1ef92e162b2c660f2f9dacfd13f1384a1
Valko Dmitry <nosefu@gmail.com>
2020-05-03 16:24:04 +0300

    Модуль 10. Задание 1. Добавил отработку исключений при обработке пользовательского ввода

--- lesson_010/01_fifth_element.py
+++ lesson_010/01_fifth_element.py
@@ -4,10 +4,28 @@
 
 BRUCE_WILLIS = 42
 
-input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
-leeloo = int(input_data[4])
-result = BRUCE_WILLIS * leeloo
-print(f"- Leeloo Dallas! Multi-pass № {result}!")
+
+# TODO подобную конструкцию я использовал для утаскивания данных с сетевых ресурсов или из последовательного порта
+#  для того, чтобы скрипт не падал, а ждал когда сервер или устройство станут доступны
+correct_input = False
+while not correct_input:
+    input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
+    try:
+        leeloo = int(input_data[4])
+    except IndexError as exc:
+        print(f'Нужно ввести не меньше 5 символов. Вы ввели {len(input_data)} символов')
+    except ValueError as exc:
+        print(f'Пятым символом должна быть цифра. У вас это "{input_data[4]}"')
+    except BaseException as exc:
+        print('Неожиданная ошибка', exc.__class__.__name__)
+        print(exc)
+    else:
+        result = BRUCE_WILLIS * leeloo
+        print(f"- Leeloo Dallas! Multi-pass № {result}!")
+        correct_input = True
+
+print('Всего доброго!')
+
 
 # Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
 # - ValueError - невозможно преобразовать к числу
