ffa00930f52fe4526849d50e593d12215b5b391d
Valko Dmitry <nosefu@gmail.com>
2020-05-12 02:46:27 +0300

    Модуль 11. Задание 2. Выполнил третью часть. Наворотил, аж сам доволен =D

--- lesson_011/02_prime_numbers.py
+++ lesson_011/02_prime_numbers.py
@@ -1,16 +1,24 @@
 # -*- coding: utf-8 -*-
+from functools import reduce
+from math import sqrt
+
+
+# TODO блин, что-то даже со школы смутно вспомнилось...
+def is_prime(number):
+    for i in range(2, int(sqrt(number)) + 1):
+        if number % i == 0:
+            # Есть делитель? Составное!
+            return False
+    return True
 
 
 # Есть функция генерации списка простых чисел
 
-
+# TODO функцию генерирования простых чисел я перепишу - так будет быстрее работать
 def get_prime_numbers(n):
     prime_numbers = []
     for number in range(2, n+1):
-        for prime in prime_numbers:
-            if number % prime == 0:
-                break
-        else:
+        if is_prime(number):
             prime_numbers.append(number)
     return prime_numbers
 
@@ -24,26 +32,26 @@ def get_prime_numbers(n):
 class PrimeNumbers:
     def __init__(self, n):
         self.numbers_count = n
-        self.prime_numbers = []
         self.last_prime = 1
 
+    def is_prime(self, number):
+        for i in range(2, int(sqrt(number)) + 1):
+            if number % i == 0:
+                return False
+        return True
+
     def __iter__(self):
-        self.prime_numbers = []
         self.last_prime = 1
         return self
 
     def __next__(self):
         for number in range(self.last_prime + 1, self.numbers_count + 1):
-            for prime in self.prime_numbers:
-                if number % prime == 0:
-                    break
-            else:
-                self.prime_numbers.append(number)
+            if self.is_prime(number):
                 self.last_prime = number
                 return number
         raise StopIteration()
 
-
+# TODO Раскомментировать. Закомментил для выполнения следующих частей, чтобы вывод не мешал и ждать не приходилось
 # prime_number_iterator = PrimeNumbers(n=10000)
 # for number in prime_number_iterator:
 #     print(number)
@@ -56,17 +64,11 @@ class PrimeNumbers:
 
 
 def prime_numbers_generator(n):
-    prime_numbers = []
     for number in range(2, n + 1):
-        for prime in prime_numbers:
-            if number % prime == 0:
-                break
-        else:
-            prime_numbers.append(number)
+        if is_prime(number):
             yield number
-    return prime_numbers
 
-
+# TODO Раскомментировать. Закомментил для выполнения следующих частей, чтобы вывод не мешал и ждать не приходилось
 # for number in prime_numbers_generator(n=10000):
 #     print(number)
 
@@ -86,10 +88,13 @@ def prime_numbers_generator(n):
 # простых счастливых палиндромных чисел и так далее. Придумать не менее 2х способов.
 #
 # Подсказка: возможно, нужно будет добавить параметр в итератор/генератор.
+# TODO не совсем понимаю, зачем параметр? Передвавть в итератор/генератор функцию фильтра что ли?
 
 
 def is_lucky(number):
     number = str(number)
+    if len(number) < 2:
+        return False
     left_half = number[:len(number) // 2]
     right_half = number[-(len(number) // 2):]
     left_sum = sum([int(digit) for digit in left_half])
@@ -104,7 +109,9 @@ def is_palindrome(number):
 # TODO хотел сначала извиниться и отделаться каким-нибудь простым алгоритмом вроде проверки числа на квадрат
 #  (находим корень от числа, а потом проверяем у него наличие дробной части с помощью math.modf),
 #  но потом наткнулся на полупростые числа (https://vk.cc/atWbrH),
-#  разработка алгоритма поиска которых показалась мне простой, но интересной
+#  разработка алгоритма поиска которых показалась мне достаточно интересной
+
+#  Вариант 1. В лоб. Медленно (хотя, после переписывания генератора уже не так медленно), неоптимально, но наглядно
 def is_semiprime(number):
     primes = []
     for prime in prime_numbers_generator(n=number):
@@ -113,7 +120,76 @@ def is_semiprime(number):
             return True
     return False
 
+#  Вариант 2. Быстрый и память не жрёт)
+def is_semiprime_v2(number):
+    # факторизируем число методом перебора делителей (во, какие умные слова я в википедии нашёл =D )
+    factors_count = 0
+    # в соответствии с методом достаточно искать до корня проверяемого числа
+    for i in range(2, int(sqrt(number)) + 1):
+        while number % i == 0:
+            number /= i
+            factors_count += 1
+        if factors_count >= 2:
+            break
+    # Если оставшееся число больше 1, значит оно простое, потому что на остальные оно не разделилось
+    if number > 1:
+        factors_count += 1
+    # если факторов больше или меньше двух, значит число не полупростое
+    return factors_count == 2
 
-print(is_lucky(92083))
-print(is_palindrome(92028))
-print(is_semiprime(920))
+
+
+# TODO способ номер 1
+# TODO Раскомментировать. Закомментил для выполнения следующих частей, чтобы вывод не мешал и ждать не приходилось
+# # Фильтруем, на выходе получаем итератор
+# lucky_semiprimes = filter(lambda x: is_lucky(x) and is_semiprime(x), range(10001))
+# print(list(lucky_semiprimes))
+# # Фильтруем, на выходе получаем генератор
+# lucky_semiprimes_2 = (x for x in range(10001) if is_lucky(x) and is_semiprime_v2(x))
+# print(list(lucky_semiprimes_2))
+
+
+# TODO Способ номер 2: в прядке общего бреда можно поробовать применить здесь декораторы
+def filtered(func, filter_func_list):
+    """
+    Декоратор применяет к выводу функции func функции-фильтры из списка filter_func_list
+
+    Parameters
+    ----------
+    func
+        функция, вывод которой нужно отфильтровать
+
+    filter_func_list
+        список функций-фильтров, возвращающих флаги
+    Returns
+    -------
+    Возвращает функцию, которая вовзращает отфильрованный вывод из func
+    """
+    if callable(filter_func_list):
+        filter_func_list = [filter_func_list, ]
+
+    def surrogate(*args, **kwargs):
+        while True:
+            gener = func(*args, **kwargs)
+            for data in gener:
+                check_data = lambda buffer, filter_func: buffer and filter_func(data)
+                filter_result = reduce(check_data, filter_func_list, True)
+                if filter_result:
+                    yield data
+            return
+    return surrogate
+
+
+# TODO Раскомментировать. Закомментил для отладки предыдущих частей
+# попробуем получить все счастливые полупростые числа в диапазоне от 5000 до 10000
+# lucky_semiprimes_fabric = filtered(range, [is_lucky, is_semiprime_v2])
+# lucky_semiprimes = lucky_semiprimes_fabric(5000, 10001)
+# for number in lucky_semiprimes:
+#     print(number)
+
+
+
+
+
+
+
