ee55a318d3557d6ee4dbabdc6d5ada0c9cb7e28a
Valko Dmitry <nosefu@gmail.com>
2020-07-11 23:30:17 +0300

    Модуль 13. Задание 1. Добавил генерацию баркода pdf417

--- lesson_013/01_ticket.py
+++ lesson_013/01_ticket.py
@@ -11,7 +11,10 @@ import random
 import string
 
 from PIL import Image, ImageFont, ImageDraw, ImageColor
+from pdf417gen import encode
+from pdf417gen import render_image as render_barcode
 from ticket_template_cp import template
+from transliterate import translit
 
 
 class TTicket:
@@ -37,8 +40,7 @@ class TTicket:
             'from': self.from_,
             'to': self.to,
             'date': self.date,
-            'flight': (random.choice(string.ascii_uppercase) + random.choice(string.ascii_uppercase)
-                       + ' ' + str(random.randint(1000, 9999))),
+            'flight': 'AC ' + str(random.randint(1000, 9999)),
             'class': self.class_,
             'seat': self.seat,
             'gate': random.choice('ABCDE') + str(random.randint(1, 99)).rjust(2, '0'),
@@ -51,13 +53,30 @@ class TTicket:
             return self.data[data_type]
         return f'{data_type} is not defined'
 
+    def _generate_barcode_data(self):
+        barcode_data = 'M1'
+        barcode_data += translit(self.full_name, 'ru', reversed=True).upper() + ' '
+        barcode_data += 'E' + ''.join([random.choice(string.ascii_uppercase + string.digits) for _ in range(0, 6)])
+        barcode_data += ' ' + translit(self.from_, 'ru', reversed=True)[:3].upper()
+        barcode_data += translit(self.to, 'ru', reversed=True)[:3].upper()
+        barcode_data += 'SU '
+        barcode_data += self.data['flight'][3:] + ' '
+        barcode_data += '006' + self.class_ + self.seat + ' '
+        barcode_data += ''.join([random.choice(string.ascii_uppercase + string.digits) for _ in range(0, 12)])
+        barcode_data += self.seat
+        return encode(barcode_data)
+
     def generate(self):
         self.image = Image.open(template.background)
         canvas = ImageDraw.Draw(self.image)
         for field in template.fields:
             if field['data_type'] == 'barcode':
+                barcode_data = self._generate_barcode_data()
+                barcode = render_barcode(barcode_data, padding=0)
+                barcode = barcode.transpose(Image.ROTATE_90)
+                barcode = barcode.resize(field['size'])
+                self.image.paste(barcode, field['pos'])
                 continue
-
             font = ImageFont.truetype(field['font'], field['font_size'])
             max_length = field['max_length']
             text = self._get_data(field['data_type'])
@@ -69,18 +88,18 @@ class TTicket:
         self.image.show()
 
     def save_to(self, file_name):
-        pass
+        self.image.save(file_name)
 
 
 def make_ticket(fio, from_, to, date):
     ticket = TTicket(fio, from_, to, date, template)
     ticket.generate()
     ticket.show()
-    ticket.save_to('new_ticket.png')
+    # ticket.save_to('new_ticket.png')
 
 
 if __name__ == '__main__':
-    make_ticket('Иванов И.И.', 'London Gatwick (LGW)', 'Naples International (NAP)', '15/02/2020')
+    make_ticket('Иванов Иван Иванович', 'London Gatwick (LGW)', 'Naples International (NAP)', '15/02/2020')
 
 # Усложненное задание (делать по желанию).
 # Написать консольный скрипт c помощью встроенного python-модуля argparse.
