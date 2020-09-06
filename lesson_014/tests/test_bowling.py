8adcb82b63e8c69220de4fb2d44295b3f4d3147f
Valko Dmitry <nosefu@gmail.com>
2020-09-06 16:03:33 +0300

    Модуль 14. Задание 1. Написал тесты для проверки корректности подсчёта очков, проверки выбрасывания исключений при передаче слишком большой последовательности фреймов, передаче некорректных символов в первой и второй позиции фрейма

---
+++ lesson_014/tests/test_bowling.py
@@ -0,0 +1,34 @@
+import unittest
+import bowling
+
+
+class BowlingTest(unittest.TestCase):
+    def test_scoring(self):
+        result = bowling.get_score('Х4/34-4')
+        # TODO везде говорится, что эначение для проверки передаётся первым параметром, аожидаемый результат - вторым.
+        #  Но если посмотреть результат теста, то будет видно,
+        #  что значение Expected берётся из первого параметра, а Actual - из второго.
+        #  Исходя из этого я и писал остальные тесты
+        self.assertEqual(46, result, 'Некорректный подсчёт очков')
+
+    def test_incorrect_symbol_first_roll(self):
+        with self.assertRaises(ValueError) as cm:
+            bowling.get_score('aХ4/34-4')
+        exc = cm.exception
+        self.assertEqual('Некорректный символ в последовательности. Состояние: FirstRoll. Полученный символ: a',
+                         exc.args[0])
+
+    def test_incorrect_symbol_second_roll(self):
+        with self.assertRaises(ValueError) as cm:
+            bowling.get_score('Х4X')
+        exc = cm.exception
+        self.assertEqual('Некорректный символ в последовательности. Состояние: SecondRoll. Полученный символ: X',
+                         exc.args[0])
+
+    def test_long_sequence(self):
+        with self.assertRaises(bowling.FrameCountError):
+            bowling.get_score('Х2/3/4/5/6/7/8/9/0/X')
+
+
+if __name__ == '__main__':
+    unittest.main()
