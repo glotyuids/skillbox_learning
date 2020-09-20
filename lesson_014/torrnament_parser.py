e8d1000e3115aa9d20159895f3186b5e2c767571
Valko Dmitry <nosefu@gmail.com>
2020-09-20 22:07:36 +0300

    Модуль 14. Задание 2. Написал класс тура: в классе считаются очки игроков (метод _get_scores()) и генерируется полный лог тура (имена игроков, ходы, очки, победитель) в виде file-like объекта.

--- lesson_014/torrnament_parser.py
+++ lesson_014/torrnament_parser.py
@@ -4,10 +4,39 @@ import io
 
 
 class Tour:
-    pass
+    def __init__(self, tour_results):
+        self.tour_results = tour_results
+        self.scores = {}
+        self._get_scores()
+        self.winner = max(self.scores, key=self.scores.get)
 
-    def get_player_results(self):
-        pass
+    def get_total_log(self):
+        """
+        Возвращает file-like текстовый объект с итоговым логом тура (входные строки, очки и победитель)
+
+        Returns
+        -------
+        total_log: io.StringIO
+        """
+        total_log = io.StringIO()
+        self.tour_results.seek(0)
+        total_log.write(self.tour_results.readline())
+        for line in self.tour_results.readlines()[1:-1]:
+            name, _ = line.split()
+            total_log.write(line[:-1] + '  \t' + str(self.scores[name]) + '\n')
+        total_log.write('winner is ' + self.winner + '\n\n')
+        total_log.seek(0)
+        return total_log
+
+    def _get_scores(self):
+        """
+        Считает очки игроков в этом туре.
+        Собирает словарь вида {имя_игрока: набранные_очки}
+        """
+        for line in self.tour_results.readlines()[1:-1]:
+            name, game_result = line.split()
+            score = get_score(game_result)
+            self.scores[name] = score
 
 
 class Tournament:
@@ -36,8 +65,12 @@ class Tournament:
             yield tour_results
 
     def run(self):
-        for tour in self.tours():
-            pass
+        for tour_results in self.tours():
+            another_tour = Tour(tour_results)
+            tour_log = another_tour.get_total_log()
+            for line in tour_log:
+                print(line, end='')
+            break
 
 
 if __name__ == '__main__':
