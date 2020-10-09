a2d5e51113887643376bf0ae22cff107e9612632
Valko Dmitry <nosefu@gmail.com>
2020-10-09 15:44:56 +0300

    Модуль 14. Задание 3. Добавил подсчёт множителей для каждого броска. Реализовал подсчёт очков по новым правилам

--- lesson_014/bowling.py
+++ lesson_014/bowling.py
@@ -6,13 +6,20 @@ class ScoreCounter:
     _state = None
     frames_count = 10
 
-    def __init__(self, game_result):
+    def __init__(self, game_result, new_rules=False):
         self.set_state(FirstRoll())
         self.game_result = game_result
+        self.new_rules = new_rules
 
         self.frame_results = []
+
+        # Вкратце по подсчёту очков по новым правилам:
+        # мы не можем заглянуть в будущее, чтобы узнать, сколько кеглей собьёт игрок в следующих двух бросках.
+        # Но мы можем сохранять на будущее множители, которые будут применяться к будущим броскам. То есть:
+        # для игры «Х4/34» список сбитых кеглей будет     [10, 4, 6, 3, 4],
+        # а список множителей, в соответствии с правилами [ 1, 2, 2, 2, 1]. В результате как раз и получим 40 очков.
         self.pins = []
-        self.multipliers = [0, 0]
+        self.multipliers = [1, 1]
 
     def set_state(self, state):
         self._state = state
@@ -23,7 +30,8 @@ class ScoreCounter:
             self._state.count_score(roll)
         if len(self.frame_results) < self.frames_count:
             raise FrameCountError(f'Количество фреймов в последовательности меньше {self.frames_count}')
-        return sum(self.frame_results)
+        return (sum(self.frame_results) if not self.new_rules
+                else sum([pins * mult for pins, mult in zip(self.pins, self.multipliers)]))
 
 
 class State:
@@ -39,9 +47,12 @@ class FirstRoll(State):
             return
         if len(self.context.frame_results) >= self.context.frames_count:
             raise FrameCountError(f'Количество фреймов в последовательности больше {self.context.frames_count}')
+        self.context.multipliers.append(1)
         if roll in 'XxХх':
             result = 20
             pins = 10
+            self.context.multipliers[-2] += 1
+            self.context.multipliers[-1] += 1
         else:
             if roll == '-':
                 result = pins = 0
@@ -61,9 +72,13 @@ class SecondRoll(State):
     def count_score(self, roll):
         if roll == '\0':
             raise IndexError('Последний фрейм не завершён - нет второго броска')
+
+        self.context.multipliers.append(1)
+
         if roll == '/':
             self.context.frame_results[-1] = 15
             pins = 10 - self.context.pins[-1]
+            self.context.multipliers[-2] += 1
         else:
             if roll == '-':
                 result = pins = 0
@@ -91,12 +106,12 @@ class SecondRoll(State):
 
 
 def get_score(game_result):
-    counter = ScoreCounter(game_result + '\0')
+    counter = ScoreCounter(game_result + '\0', new_rules=True)
     return counter.get_score()
 
 
 if __name__ == '__main__':
     # ваше решение не обрабатывает след. неправильные для 10 фреймовой игры по 10 кеглей на фрейм данные
-    input = 'X' * 3 + '252/' + 'X' * 5
+    input = 'Х4/34--------------'
     #input = 'X' * 9 + '55'
     print(get_score(input))
