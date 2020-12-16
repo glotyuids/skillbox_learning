5aafdd28c2355c5fdd816a9809c6a22bc8dec4d0
Valko Dmitry <nosefu@gmail.com>
2020-12-16 23:23:39 +0300

    Курсовой проект. Практика. Поправил форматирование RAW_EVENT

--- chatbot/tests/test_bot.py
+++ chatbot/tests/test_bot.py
@@ -9,15 +9,23 @@ import vk_bot
 
 
 class VKBotTestCase(unittest.TestCase):
-    RAW_EVENT = {'type': 'message_new',
-                 'object': {'message': {'date': 1602440563, 'from_id': 25833442, 'id': 86, 'out': 0,
-                                        'peer_id': 25833442, 'text': 'Hello', 'conversation_message_id': 84,
-                                        'fwd_messages': [], 'important': False, 'random_id': 0, 'attachments': [],
-                                        'is_hidden': False},
-                            'client_info': {'button_actions': ['text', 'vkpay', 'open_app', 'location', 'open_link'],
-                                            'keyboard': True, 'inline_keyboard': True, 'carousel': False, 'lang_id': 0}},
-                 'group_id': 197090073,
-                 'event_id': 'cda2978fe40bb257ba476407ad8ca57c5d3c6b08'}
+    RAW_EVENT = {
+        'type': 'message_new',
+        'object': {
+            'message': {
+                'date': 1602440563, 'from_id': 25833442, 'id': 86, 'out': 0,
+                'peer_id': 25833442, 'text': 'Hello', 'conversation_message_id': 84,
+                'fwd_messages': [], 'important': False, 'random_id': 0, 'attachments': [],
+                'is_hidden': False
+            },
+            'client_info': {
+                'button_actions': ['text', 'vkpay', 'open_app', 'location', 'open_link'],
+                'keyboard': True, 'inline_keyboard': True, 'carousel': False,
+                'lang_id': 0
+            }
+        },
+        'group_id': 197090073,
+        'event_id': 'cda2978fe40bb257ba476407ad8ca57c5d3c6b08'}
 
     def test_run(self):
         count = 5
