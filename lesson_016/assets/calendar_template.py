51335242c6f29c3012a02e6586e46f1492dd5cca
Valko Dmitry <nosefu@gmail.com>
2021-03-10 23:03:47 +0300

    refactor(lesson_16): html-код шаблона календаря вынесен в отдельный модуль

---
+++ lesson_016/assets/calendar_template.py
@@ -0,0 +1,135 @@
+header = """
+    <html>
+      <head>
+        <meta charset="utf-8">
+      </head>
+    
+      <body>
+"""
+footer = """
+    </body>
+  </html>
+"""
+css = """
+
+    <style type="text/css">
+      @import url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;500&display=swap');
+
+      # @font-face {
+      #   font-family: 'Weather Icons';
+      #   src: url('assets/weathericons-regular-webfont.woff') format('woff'),
+      #   url('assets/weathericons-regular-webfont.ttf') format('truetype');
+      # }
+
+      @font-face {
+      font-family: 'Weather Icons'; 
+      url('https://erikflowers.github.io/weather-icons/font/weathericons-regular-webfont.woff') format('woff'), 
+      url('https://erikflowers.github.io/weather-icons/font/weathericons-regular-webfont.ttf') format('truetype'), 
+      font-weight: normal;
+      font-style: normal;
+    }
+
+      body {
+        color: rgb(51, 51, 51);
+        font-family: "Roboto Slab";
+        width: 857px;
+      }
+
+      .wrapper {
+        width: 857px;
+      }
+
+      table {
+        border-collapse: collapse;
+      }
+
+      .month_name {
+        font-size: 30;
+        text-align: left;
+        padding-left: 12;
+        padding-bottom: 6;
+      }
+      th.city {
+        font-size: 30;
+        text-align: right;
+         padding-right: 12;
+        padding-bottom: 6;
+      }
+
+      tr:nth-child(2) {
+        line-height: 1.5;
+        font-size: 20;
+      }
+
+      .noday {
+        height: 142px;
+        width: 120px;
+      }
+
+      .day {
+        height: 142px;
+        width: 120px;
+        border: 1px solid rgb(51, 51, 51);
+      }    
+
+      .sat > .date,  .sun > .date, th.sat, th.sun { 
+        color: #ff4949;
+      }
+
+      .sun.day, .sat.day { 
+        border-color: #ff4949;
+      }
+
+      .fri {
+        border-right-width: 0;
+      }
+
+      .fri + .noday {
+        border-left: 1px solid rgb(51 51 51);
+      }
+
+      .date {
+        margin-top: -10px;
+        padding-right: 10px;
+        font-size: 24px;
+        line-height: 1.528;
+        text-align: right;
+      }
+
+      .icon {
+        padding-top: 10px;
+        font-size: 34px;
+        font-family: "Weather Icons";
+        line-height: 0.809;
+        text-align: center;
+      }
+
+      .descr {
+        font-size: 14.667px;
+        line-height: 2.5;
+        text-align: center;
+      }
+
+      .temp {
+        font-size: 20px;
+        line-height: 1.2;
+        text-align: center;
+        left: 17.625px;
+        top: 102.17px;
+        z-index: 4;
+      }
+
+    </style>
+    """
+
+month_header = '<tr><th colspan="3" class="month_name">{month}</th>' \
+               '<th colspan="4" class="city">{city}</th></tr>'
+
+day = """
+  <td class="{weekday} day">
+    <div class="date">{day}</div>
+    <div class="icon">{weather_icon}</div>
+    <div class="descr">{descr}</div>
+    <div class="temp">{temp_day}°  {temp_night}°</div>
+  </td>
+"""
--- lesson_016/engine.py
+++ lesson_016/engine.py
@@ -12,6 +12,7 @@ import cv2
 import imgkit
 import requests
 
+from assets import calendar_template as html_tmpl
 from assets import detailed_big_template as template
 
 
@@ -163,130 +164,6 @@ class CalendarMaker(calendar.HTMLCalendar):
         11: 'ноябрь',
         12: 'декабрь',
     }
-    header = """
-    <html>
-      <head>
-        <meta charset="utf-8">
-      </head>
-
-      <body>
-
-      """
-    footer = """
-      </body>
-    </html>
-    """
-    css = """
-
-    <style type="text/css">
-      @import url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;500&display=swap');
-
-      # @font-face {
-      #   font-family: 'Weather Icons';
-      #   src: url('assets/weathericons-regular-webfont.woff') format('woff'),
-      #   url('assets/weathericons-regular-webfont.ttf') format('truetype');
-      # }
-
-      @font-face {
-      font-family: 'Weather Icons'; 
-      url('https://erikflowers.github.io/weather-icons/font/weathericons-regular-webfont.woff') format('woff'), 
-      url('https://erikflowers.github.io/weather-icons/font/weathericons-regular-webfont.ttf') format('truetype'), 
-      font-weight: normal;
-      font-style: normal;
-    }
-
-      body {
-        color: rgb(51, 51, 51);
-        font-family: "Roboto Slab";
-        width: 857px;
-      }
-
-      .wrapper {
-        width: 857px;
-      }
-
-      table {
-        border-collapse: collapse;
-      }
-
-      .month_name {
-        font-size: 30;
-        text-align: left;
-        padding-left: 12;
-        padding-bottom: 6;
-      }
-      th.city {
-        font-size: 30;
-        text-align: right;
-         padding-right: 12;
-        padding-bottom: 6;
-      }
-
-      tr:nth-child(2) {
-        line-height: 1.5;
-        font-size: 20;
-      }
-
-      .noday {
-        height: 142px;
-        width: 120px;
-      }
-
-      .day {
-        height: 142px;
-        width: 120px;
-        border: 1px solid rgb(51, 51, 51);
-      }    
-
-      .sat > .date,  .sun > .date, th.sat, th.sun { 
-        color: #ff4949;
-      }
-
-      .sun.day, .sat.day { 
-        border-color: #ff4949;
-      }
-
-      .fri {
-        border-right-width: 0;
-      }
-
-      .fri + .noday {
-        border-left: 1px solid rgb(51 51 51);
-      }
-
-      .date {
-        margin-top: -10px;
-        padding-right: 10px;
-        font-size: 24px;
-        line-height: 1.528;
-        text-align: right;
-      }
-
-      .icon {
-        padding-top: 10px;
-        font-size: 34px;
-        font-family: "Weather Icons";
-        line-height: 0.809;
-        text-align: center;
-      }
-
-      .descr {
-        font-size: 14.667px;
-        line-height: 2.5;
-        text-align: center;
-      }
-
-      .temp {
-        font-size: 20px;
-        line-height: 1.2;
-        text-align: center;
-        left: 17.625px;
-        top: 102.17px;
-        z-index: 4;
-      }
-
-    </style>
-    """
 
     def formatday(self, day, weekday, **kwargs):
         """
@@ -311,14 +188,8 @@ class CalendarMaker(calendar.HTMLCalendar):
             return '<td class="noday">&nbsp;</td>' \
                    % {"weekday": self.cssclasses[weekday]}
 
-        return """  <td class="{weekday} day">
-                      <div class="date">{day}</div>
-                      <div class="icon">{weather_icon}</div>
-                      <div class="descr">{descr}</div>
-                      <div class="temp">{temp_day}°  {temp_night}°</div>
-          </td>""".format(weekday=self.cssclasses[weekday], day=day,
-                          weather_icon=template.ICONS[stat.descr],
-                          **stat.dict)
+        return html_tmpl.day.format(weekday=self.cssclasses[weekday], day=day,
+                                    weather_icon=template.ICONS[stat.descr], **stat.dict)
 
     def formatweek(self, theweek, **kwargs):
         """
@@ -381,8 +252,7 @@ class CalendarMaker(calendar.HTMLCalendar):
             s = '%s %s' % (self.month_name[themonth], theyear)
         else:
             s = '%s' % self.month_name[themonth]
-        return '<tr><th colspan="3" class="month_name">{month}</th>' \
-               '<th colspan="4" class="city">{city}</th></tr>'.format(month=s, city=city)
+        return html_tmpl.month_header.format(month=s, city=city)
 
     def formatrange(self, stats):
         """
@@ -394,14 +264,14 @@ class CalendarMaker(calendar.HTMLCalendar):
         """
         start_date = stats[0].date
         end_date = stats[-1].date
-        cal = self.header + self.css
+        cal = html_tmpl.header + html_tmpl.css
         cal += '<div class="wrapper"><br>'
         for month in rrule.rrule(rrule.MONTHLY, dtstart=start_date, until=end_date):
             with setlocale(locale.LC_ALL, 'ru_RU.UTF-8'):
                 cal += self.formatmonth(theyear=month.year, themonth=month.month, withyear=True, stats=stats)
                 cal += '<br><br>'
         cal = cal[:-4] + '</div>'
-        cal += self.footer
+        cal += html_tmpl.footer
         return cal
 
 
