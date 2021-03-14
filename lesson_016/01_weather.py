83c9bacbcc9121185fd71ac3480ce8e70d11e025
Valko Dmitry <nosefu@gmail.com>
2021-03-14 18:10:39 +0300

    feat(lesson_16): начал делать консольный интерфейс утилиты

--- lesson_016/01_weather.py
+++ lesson_016/01_weather.py
@@ -46,3 +46,156 @@
 # Приконнектится по полученному url-пути к базе данных
 # Инициализировать её через DatabaseProxy()
 # https://peewee.readthedocs.io/en/latest/peewee/database.html#dynamically-defining-a-database
+
+from abc import abstractmethod
+import datetime as dt
+
+from tabulate import tabulate
+
+import engine
+
+
+class Utility:
+    def __init__(self, db_url=None):
+        self.weather = self.get_weather()
+        self.db = engine.DatabaseUpdater(db_url or 'sqlite:///weather.db')
+        self.start_date = dt.datetime.now().date() - dt.timedelta(days=7)
+        self.end_date = dt.datetime.now().date()
+        self.stats = None
+        self.state = None
+        self.wants_exit = False
+        self.set_state(MainMenu)
+
+    def set_state(self, state):
+        self.state = state()
+        self.state.context = self
+
+    def get_weather(self):
+        weather = None
+        while not weather:
+            city = input('Введите ваш город: ')
+            try:
+                weather = engine.WeatherMaker(city)
+            except engine.BadResponseException as exc:
+                print(exc)
+                print('Проверьте соединение с интернетом')
+            except engine.EmptyResponseException as exc:
+                print(exc)
+        return weather
+
+    def print_weather(self):
+        data = [list(stat.dict.values()) for stat in self.stats]
+        for stat in data:
+            del stat[0]
+        print(tabulate(data, tablefmt="pretty",
+                       headers=['Дата', 'Дневная t', 'Ночная t', 'Погода',
+                                'Давление', 'Влажность', 'Скорость ветра', 'Направление ветра']))
+
+    def run(self):
+        self.stats = self.db.get_stats(city=self.weather.city,
+                                       start_date=self.start_date,
+                                       end_date=self.end_date)
+        if len(self.stats) < 7:
+            self.stats = self.weather.get_range(self.start_date, self.end_date)
+            self.db.add_stats(self.stats)
+        self.print_weather()
+        while not self.wants_exit:
+            self.state.menu()
+
+
+class Menu:
+    def __init__(self):
+        self.context = None
+        self.avail_actions = None
+
+    def print_actions(self):
+        for _, action in self.avail_actions.items():
+            print(action['text'])
+
+    def handle_input(self):
+        while True:
+            user_input = input('>: ')
+            if user_input not in self.avail_actions.keys():
+                print('Неверная команда. Попробуйте ещё раз:')
+            else:
+                break
+        payload = self.avail_actions[user_input]['payload']
+        args = self.avail_actions[user_input]['payload_args']
+        payload(*args)
+
+    @abstractmethod
+    def menu(self):
+        pass
+
+
+class MainMenu(Menu):
+    def get_avail_actions(self):
+        self.avail_actions = {
+            # '1': {
+            #     'text': '1. Добавить прогнозы в базу данных',
+            #     'payload': self.context.set_state,
+            #     'payload_args': [AddToDBMenu]
+            # },
+            # '2': {
+            #     'text': '2. Получить прогнозы из базы данных',
+            #     'payload': self.context.set_state,
+            #     'payload_args': [GetFromDBMenu]
+            # },
+            # '3': {
+            #     'text': '3. Создать изображения из полученных прогнозов',
+            #     'payload': self.context.set_state,
+            #     'payload_args': [GetImagesMenu]
+            # },
+            '4': {
+                'text': '4. Вывести прогноз на консоль',
+                'payload': self.context.print_weather,
+                'payload_args': []
+            },
+            '5': {
+                'text': '5. Выйти из программы',
+                'payload': self.context.set_state,
+                'payload_args': [ExitMenu]
+            }
+        }
+
+
+    def menu(self):
+        print(f'Текущий диапазон: {self.context.stats[0].date} - {self.context.stats[-1].date}')
+        print('Выберите действие:')
+        self.get_avail_actions()
+        self.print_actions()
+        self.handle_input()
+
+
+class ExitMenu(Menu):
+    def exit_handler(self):
+        self.context.wants_exit = True
+
+    def get_avail_actions(self):
+        self.avail_actions = {
+            '1': {
+                'text': f'1. Нет, вернуться назад',
+                'enabled': True,
+                'payload': self.context.set_state,
+                'payload_args': [MainMenu]
+            },
+            '2': {
+                'text': f'2. Да, выйти из программы',
+                'enabled': True,
+                'payload': self.exit_handler,
+                'payload_args': []
+            }
+        }
+
+    def menu(self):
+        self.get_avail_actions()
+        print('\nВы уверены?')
+        self.print_actions()
+        self.handle_input()
+
+
+if __name__ == '__main__':
+    util = Utility()
+    util.run()
+
+
