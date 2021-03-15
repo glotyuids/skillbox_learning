5af19bbae4593cc483b12d418d018188e5f2b3ff
Valko Dmitry <nosefu@gmail.com>
2021-03-15 21:16:32 +0300

    docs(lesson_16): документирование основного модуля

--- lesson_016/01_weather.py
+++ lesson_016/01_weather.py
@@ -58,6 +58,7 @@ import engine
 
 
 class UserInterface:
+    """ Реализует необходимый функционал для работы консольного интерфейса """
     def __init__(self, db_url=None):
         self.wants_exit = False
         self.state = None
@@ -71,10 +72,12 @@ class UserInterface:
         self.set_state(MainMenu)
 
     def set_state(self, state):
+        """ Меняет своё состояние и контекст у меню """
         self.state = state()
         self.state.context = self
 
     def get_weather(self):
+        """ Запрашивает у пользоваетеля город, ищет его на сервере и создаёт новый WeatherMaker """
         weather = None
         while not weather:
             city = input('Введите город: ')
@@ -88,6 +91,7 @@ class UserInterface:
         self.weather = weather
 
     def print_weather(self):
+        """ Выводит на консоль таблицу с погодой за текущий период """
         data = [list(stat.dict.values()) for stat in self.stats]
         for stat in data:
             del stat[0]
@@ -96,6 +100,11 @@ class UserInterface:
                                 'Давление', 'Влажность', 'Скорость ветра', 'Направление ветра']))
 
     def get_last_week(self):
+        """
+        Тянет из бд погоду за последнюю неделю
+        (если нет, то сначала тащит с сервера и кидает в бд)
+        и выводит её на консоль в виде таблицы
+        """
         self.start_date = dt.datetime.now().date() - dt.timedelta(days=6)
         self.end_date = dt.datetime.now().date()
         self.stats = self.db.get_stats(city=self.weather.city,
@@ -107,21 +116,25 @@ class UserInterface:
         self.print_weather()
 
     def run(self):
+        """ Запускает пользовательский интерфейс """
         self.get_last_week()
         while not self.wants_exit:
             self.state.menu()
 
 
 class Menu:
+    """ Базовый класс для меню """
     def __init__(self):
         self.context = None
         self.avail_actions = None
 
     def print_actions(self):
+        """ Выводит на консоль список доступных действий """
         for _, action in self.avail_actions.items():
             print(action['text'])
 
     def handle_input(self):
+        """ Обрабатывает пользовательский ввод в меню, запускает хендлеры """
         while True:
             user_input = input('>: ')
             if user_input not in self.avail_actions.keys():
@@ -133,6 +146,7 @@ class Menu:
         payload(*args)
 
     def set_dates(self):
+        """ Запрашивает у пользователя диапазон дат и задаёт этот диапазон в контексте """
         while True:
             user_input = input('>: ')
             if user_input.lower() == 'назад':
@@ -155,11 +169,19 @@ class Menu:
 
     @abstractmethod
     def menu(self):
-        pass
+        """ Выводит текущее меню и обрабатывает пользовательский ввод """
+
+    def get_avail_actions(self):
+        """ Здесь задаётся список доступных пунктов меню и их хендлеры """
 
 
 class MainMenu(Menu):
+    """ Главное меню """
     def get_new_city(self):
+        """
+        Запрашивает у пользователя город, задаёт его контексту
+        и выводит прогноз за последнюю неделю на консоль
+        """
         self.context.get_weather()
         self.context.get_last_week()
 
@@ -208,7 +230,9 @@ class MainMenu(Menu):
 
 
 class ExitMenu(Menu):
+    """ Меню выхода из программы """
     def exit_handler(self):
+        """ Хендлер выхода из программы"""
         self.context.wants_exit = True
 
     def get_avail_actions(self):
@@ -233,8 +257,12 @@ class ExitMenu(Menu):
 
 
 class AddToDBMenu(Menu):
-
+    """ Меню добавления данных в бд """
     def menu(self):
+        """
+        Запрашивает у пользователя диапазон дат,
+        тянет с сервера погоду за этот диапазон и заливает её в бд
+        """
         context = self.context
         print('\nВведите дату, либо диапазон дат через косую черту')
         print('в формате дд-мм-гггг/дд-мм-гггг, '
@@ -255,7 +283,12 @@ class AddToDBMenu(Menu):
 
 
 class GetFromDBMenu(Menu):
+    """ Меню запроса данных из бд """
     def menu(self):
+        """
+        Запрашивает у пользователя диапазон дат, тянет из бд погоду за этот диапазон.
+        Если погоды нет - ругается и просит выбрать другой диапазон
+        """
         context = self.context
         print('\nВведите дату, либо диапазон дат через косую черту')
         print('в формате дд-мм-гггг/дд-мм-гггг, '
@@ -279,7 +312,13 @@ class GetFromDBMenu(Menu):
 
 
 class GetImagesMenu(Menu):
+    """ Меню генерации картинок """
     def create_cards(self):
+        """
+        Спрашивает у пользователя место для сохранения
+        и сохраняет погоду в отдельные карточки для каждого дня.
+        При пустом вводе сохраняет в папку cards рядом со скриптом
+        """
         print('\nКуда будем сохранять изображения? '
               'Введите путь, либо оставьте пустым, чтобы сохранить в папку cards рядом со скриптом')
         user_input = input('>: ')
@@ -297,6 +336,11 @@ class GetImagesMenu(Menu):
         print(f'Все изображения сохранены в {path}')
 
     def create_calendar(self):
+        """
+        Спрашивает у пользователя место для сохранения
+        и сохраняет погоду в картинку - общий календарь.
+        При пустом вводе сохраняет в папку рядом со скриптом
+        """
         print('\nКуда будем сохранять изображения? '
               'Введите путь, либо оставьте пустым, чтобы сохранить в папку рядом со скриптом')
         user_input = input('>: ')
